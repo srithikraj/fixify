8d9ca53e35cb0a466480086945f4c2126267db38
Rithik Sowdermett <srithik2310@gmail.com>
2025-02-08 23:05:39 -0500

    added setup.sh, updated README instructions, moved gitignore to repo's root

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# Simply run `./setup.sh`
+
+# Install node dependencies for frontend app
+cd source_code/frontend
+npm install
+
+# Install node dependencies for backend server
+cd ../backend
+npm install
+
+# Return root of repository
+cd ../..
