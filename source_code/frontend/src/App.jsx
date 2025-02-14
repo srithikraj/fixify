447b2ab683717b743a71dfa054a50172d1077940
Rajveer Sinh Jadav <rajveersinhjadav@rajveers-air.home>
2025-02-13 20:45:47 -0500

    aboutus page

--- source_code/frontend/src/App.jsx
+++ source_code/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Home from './pages/home'
 import { BrowserRouter, Route, Routes, Router } from "react-router-dom";
 import SignupCustomer from './pages/signupCustomer';
+import Aboutus from './pages/aboutUs';
 const App = () => {
   return (
     <div>
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/create-account" element={<SignupCustomer />} />
           <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<Aboutus />} />
         </Routes>
       </BrowserRouter>
     </div>
