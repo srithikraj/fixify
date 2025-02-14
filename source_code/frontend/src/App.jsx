9074ba863545973141040a0323ce77d4c2cb1dbd
Rajveer Sinh Jadav <rajveersinhjadav@rajveers-air.home>
2025-02-13 20:45:47 -0500

    aboutus page

--- source_code/frontend/src/App.jsx
+++ source_code/frontend/src/App.jsx
@@ -5,10 +5,6 @@ import { BrowserRouter, Route, Routes, Router } from "react-router-dom";
 import MainLayout from './MainLayout';
 import SignupCustomer from './pages/signupCustomer';
 import Aboutus from './pages/aboutUs';
-import OTPVerification from './pages/CustomerVerification';
-import LoginPage from './pages/signin'; // Import LoginPage
-import WorkerManagementForm from './pages/serviceProviderLogin'; // Import the service provider page
-
 const App = () => {
   return (
     <div>
@@ -17,9 +13,6 @@ const App = () => {
           <Route path="/create-account" element={<SignupCustomer />} />
           <Route path="/" element={<Home />} />
           <Route path="/aboutus" element={<Aboutus />} />
-          <Route path="/verify-customer" element={<OTPVerification />} />
-          <Route path="/signin" element={<LoginPage />} /> {/* Add Sign-in Route */}
-          <Route path="/service-provider" element={<WorkerManagementForm />} />
         </Routes>
       </BrowserRouter>
     </div>
