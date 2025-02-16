a6efc544894a9fc49d4db3a8969472ede8517c00
Arshita <64703962+arshita1625@users.noreply.github.com>
2025-02-15 23:32:05 -0500

    fixed navbar in route

--- source_code/frontend/src/pages/home.jsx
+++ source_code/frontend/src/pages/home.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import Footer from '../components/Footer/footer';
 import Hero from '../components/Hero/hero';
 import HowItWorks from '../components/howitworks/howworks';
-import Navbar from '../components/Navbar/Navbar';
 import ReadyToGetStarted from '../components/getstarted/getstarted';
 import ServicesSection from '../components/serviceSection/serviceSection';
 import WorkerManagementForm from "./serviceProviderMgmt";
-
-
+import Navbar from '../components/Navbar/navbar';
 function Home() {
   return (
     <>
