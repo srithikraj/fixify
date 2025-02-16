c9f9e310eb2227a675abeafc87dd52015fe11fe5
Arshita <64703962+arshita1625@users.noreply.github.com>
2025-02-15 23:03:40 -0500

    added navigate

--- source_code/frontend/src/pages/home.jsx
+++ source_code/frontend/src/pages/home.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import Footer from '../components/Footer/footer'
-import Hero from '../components/Hero/hero'
-import HowItWorks from '../components/howitworks/howworks'
-import Navbar from '../components/Navbar/Navbar'
-import ReadyToGetStarted from '../components/getstarted/getstarted'
-import ServicesSection from '../components/serviceSection/serviceSection'
-import WorkerManagementForm from "./serviceProviderMgmt"
+import React from 'react';
+import Footer from '../components/Footer/footer';
+import Hero from '../components/Hero/hero';
+import HowItWorks from '../components/howitworks/howworks';
+import Navbar from '../components/Navbar/Navbar';
+import ReadyToGetStarted from '../components/getstarted/getstarted';
+import ServicesSection from '../components/serviceSection/serviceSection';
+import WorkerManagementForm from "./serviceProviderMgmt";
 
 
 function Home() {
