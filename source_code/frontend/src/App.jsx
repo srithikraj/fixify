5d3987f609c2191aee94d54935c242fe39fbec8a
Geetika Hanumara <ghanumar@uwaterloo.ca>
2025-02-18 13:35:13 -0500

    routingSetup

--- source_code/frontend/src/App.jsx
+++ source_code/frontend/src/App.jsx
@@ -9,33 +9,17 @@ import OTPVerification from './pages/CustomerVerification';
 import LoginPage from './pages/signin'; // Import LoginPage
 import WorkerManagementForm from './pages/serviceProviderMgmt'; // Import the service provider page
 
-import AdminDashboard from './pages/AdminDashboard';
-import ManageCustomers from './pages/AdminManageCustomers';
-import ManageWorkers from './pages/AdminManageWorkers';
-import AdminLayout from './pages/AdminLayout';
-import SignupChoice from './pages/SignupChoice';
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={<MainLayout />}>
-            <Route path="/create-account" element={<SignupCustomer />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/aboutus" element={<Aboutus />} />
-            <Route path="/verify-customer" element={<OTPVerification />} />
-            <Route path="/signin" element={<LoginPage />} /> {/* Add Sign-in Route */}
-            <Route path="/service-provider" element={<WorkerManagementForm />} />
-            <Route path="/signup-choice" element={<SignupChoice />} />
-        </Route>
-
-
-
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<AdminDashboard />} />
-            <Route path="customers" element={<ManageCustomers />} />
-            <Route path="workers" element={<ManageWorkers />} />
-        </Route>
+          <Route path="/create-account" element={<SignupCustomer />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/verify-customer" element={<OTPVerification />} />
+          <Route path="/signin" element={<LoginPage />} /> {/* Add Sign-in Route */}
+          <Route path="/service-provider" element={<WorkerManagementForm />} />
         </Routes>
       </BrowserRouter>
     </div>
