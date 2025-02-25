8253e05f8039c4e8b7f3233291dfe2d5b1b4143c
Arshita <64703962+arshita1625@users.noreply.github.com>
2025-02-24 23:28:13 -0500

    service provider api integration

--- source_code/frontend/src/api/serviceProviderApi.js
+++ source_code/frontend/src/api/serviceProviderApi.js
@@ -1,3 +1,12 @@
 import axios from "axios";
 
 const URL = "http://localhost:3000"
+export async function getAllServiceProviders() {
+    const response = await axios.get(`${URL}/serviceProviders`)
+
+    if (response.status == 200) {
+        return response
+    } else {
+        return
+    }
+}
