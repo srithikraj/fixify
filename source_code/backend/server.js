78c83d0e0dc60662db4d2b68b5e4e19412173a16
Rithik Sowdermett <srithik2310@gmail.com>
2025-02-26 00:01:03 -0500

    refactor server.js and connect.js, impl singleton patten for db connection

--- source_code/backend/server.js
+++ source_code/backend/server.js
@@ -1,29 +1,52 @@
-const connect = require("./connect")
-const express = require("express")
-const cors = require("cors")
-const users = require("./routes/userRoutes")
-const services = require("./routes/serviceRoutes")
-const serviceProviders = require("./routes/serviceProviderRoutes")
-const reviews = require("./routes/reviewRoutes")
+const express = require("express");
+const cors = require("cors");
+const connect = require("./connect");
+const users = require("./routes/userRoutes");
+const services = require("./routes/serviceRoutes");
+const serviceProviders = require("./routes/serviceProviderRoutes");
+const reviews = require("./routes/reviewRoutes");
+const logger = require("./logger");
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use(users)
-app.use(services)
-app.use(serviceProviders)
-app.use(reviews)
+app.use(users);
+app.use(services);
+app.use(serviceProviders);
+app.use(reviews);
 
+let server;
 
-connect.connectToServer().then(() => {
-    console.log("BACKEND: Successfully connected to DB!")
-    app.listen( PORT, () => {
-        console.log(`BACKEND: Successfully started backend server! Listening on port ${PORT}`)
-    })
-}).catch((error) => {
-    console.log("BACKEND: Failed to connect to DB!")
-    console.log(error)
-})
+const startServer = async () => {
+  try {
+    await connect.connectToServer();
+    logger.log("Successfully connected to DB!");
+
+    server = app.listen(PORT, () => {
+      logger.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    logger.error("Failed to connect to DB!", error);
+    process.exit(1);
+  }
+};
+
+// Graceful shutdown server
+const shutdownServer = async () => {
+  logger.log("Shutting down server...");
+  await connect.closeConnection();
+  if (server) {
+    server.close(() => {
+      logger.log("Server closed.");
+      process.exit(0);
+    });
+  }
+};
+
+process.on("SIGINT", shutdownServer);
+process.on("SIGTERM", shutdownServer);
+
+startServer();
