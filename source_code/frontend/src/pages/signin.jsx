3b7eccd60ed99ce4e617c3f71b37d08bf2e16a4a
Bhargav parekh <39110031+parekhbhargav25@users.noreply.github.com>
2025-02-26 17:56:56 -0500

    Update signin.jsx
    resolved sign in conflict

--- source_code/frontend/src/pages/signin.jsx
+++ source_code/frontend/src/pages/signin.jsx
@@ -15,6 +15,7 @@ import { Container } from "@mui/system";
 import React, { useState } from "react";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Box from '@mui/material/Box';
+import Grid from "@mui/material/Grid";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { verifyUser } from '../api/userApi'
@@ -49,52 +50,6 @@ const LoginPage= () => {
     }
    
   }
-  
-  return(
-    <Container maxWidth="xs">
-      <Paper elevation={10} sx={ {marginTop:8,padding:2}}>
-        <Avatar
-          sx={{
-            mx: "auto",
-            bgcolor:"secondary main",
-            textAlign:"center",
-            mb:1,
-            
-          }}>
-            <LockOutlinedIcon/>
-        </Avatar>
-        <Typography component="h1" variant="h5" sx={{textAlign:"center"}}>
-          Sign In
-        </Typography>
-        <Box component="form" 
-          onSubmit={handleSubmit}
-          noValidate
-          sx={{mt:1}}
-        >
-          <TextField 
-          placeholder="Enter username" 
-          onChange={handleChange}
-          name="username"
-          fullWidth required autoFocus 
-          sx={{mb :2}}>
-          </TextField>
-          <TextField 
-          placeholder="Enter password" 
-          name="password"
-          onChange={handleChange}
-          fullWidth 
-          required 
-          type="password">
-          </TextField>
-          <FormControlLabel control={<Checkbox value="remember" color="primary"/>}
-          label="Remember me">
-          </FormControlLabel>   
-          <Button type="submit" variant    ="contained" fullWidth sx={{mt:1}}>
-            Sign In
-          </Button>
-        </Box>
-      </Paper>
-    </Container>
 
   return (
     <Grid container component="main" sx={{ height: "100vh" }}>
