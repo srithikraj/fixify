95675fcbdab73805411e038b7289fa769a970871
Bhargav Parekh <parekhbhargav25@gmail.com>
2025-03-04 17:38:52 -0500

    trying to fix commit history
    added OTP and other features
    
    added nodemailer
    
    fixed nodmailer issue
    
    fixed nodmailer issue
    
    fixed nodmailer issue
    
    Rajveers test cases

--- source_code/frontend/src/pages/SignupChoice.jsx
+++ source_code/frontend/src/pages/SignupChoice.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React,{ useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box, Typography, Card, CardContent, Button, IconButton } from "@mui/material";
 import { FaUser, FaBriefcase } from "react-icons/fa";
