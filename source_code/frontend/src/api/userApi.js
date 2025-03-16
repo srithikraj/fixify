721a3146a04f591e13160480d03805e9e62839d7
Arshita <64703962+arshita1625@users.noreply.github.com>
2025-03-16 14:16:18 -0400

    fetch all customers api

--- source_code/frontend/src/api/userApi.js
+++ source_code/frontend/src/api/userApi.js
@@ -2,10 +2,16 @@ import axios from "axios";
 
 const URL = "http://localhost:3000"
 
-/**
- * User Login
- */
 export async function verifyUser(user) {
     const response = await axios.post(`${URL}/users/login`, user)
     return response.data
+}
+
+export async function fetchAllUsers() {
+    const response = await axios.get(`${URL}/users`)
+    if (response.status == 200) {
+        return response
+    } else {
+        return
+    }
 }
