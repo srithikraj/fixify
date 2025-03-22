6f6edcf26f513438d313f10703c0fc31b8aba59f
Geetika Hanumara <ghanumar@uwaterloo.ca>
2025-03-22 19:37:28 -0400

    display top reviews

--- source_code/frontend/src/pages/home.jsx
+++ source_code/frontend/src/pages/home.jsx
@@ -6,9 +6,20 @@ import Navbar from '../components/Navbar/Navbar';
 import ReadyToGetStarted from '../components/getstarted/getstarted';
 import ServicesSection from '../components/serviceSection/serviceSection';
 import WorkerManagementForm from "./serviceProviderSignup";
+import { useEffect,useState } from "react";
+import { getTopReviews } from '../api/reviewApi';
+import { Container, Grid, Typography } from "@mui/material";
+import ReviewCard from "../components/ReviewCard.jsx";
+
 
 
 function Home() {
+  const [reviews, setReviews] = useState([]);
+    useEffect(() => {
+      getTopReviews().then((data) => {
+        setReviews(data);
+      });
+    }, []);
   return (
     <>
       <Navbar />
@@ -16,6 +27,18 @@ function Home() {
       <HowItWorks />
       <ServicesSection />
       <ReadyToGetStarted />
+      <Container maxWidth="lg" sx={{ my: 5 }}>
+        <Typography variant="h4" align="center" gutterBottom sx={{ fontWeight: 'bold' }}>
+          ⭐ Customer Reviews ⭐
+        </Typography>
+        <Grid container spacing={2} justifyContent="center">
+          {reviews.map((review, index) => (
+            <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
+              <ReviewCard review={review} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
       <Footer />
     </>
   )
