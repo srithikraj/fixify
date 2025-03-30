16fb7683a7d9f43f586609ab4a1db6838b5866b3
Rithik Sowdermett <srithik2310@gmail.com>
2025-03-30 19:02:12 -0400

    Add frontend api method to fetch all verified service providers

--- source_code/frontend/src/api/serviceProviderApi.js
+++ source_code/frontend/src/api/serviceProviderApi.js
@@ -7,6 +7,29 @@ export async function getAllServiceProviders() {
     if (response.status == 200) {
         return response
     } else {
+        console.error("Error fetching profile data:", error);
+        return
+    }
+}
+
+// export async function getServiceProvidersByCity(city) {
+//     const response = await axios.get(`${URL}/serviceProviders?city=${city}`)
+
+//     if (response.status == 200) {
+//         return response
+//     } else {
+//         console.error("Error fetching profile data:", error);
+//         return
+//     }
+// }
+
+export async function getVerifiedServiceProviders() {
+    const response = await axios.get(`${URL}/verified/serviceProviders`)
+
+    if (response.status == 200) {
+        return response
+    } else {
+        console.error("Error fetching profile data:", error);
         return
     }
 }
