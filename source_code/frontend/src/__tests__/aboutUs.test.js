82bc664abaae0ce0036e022b696efd0699548c90
Raghul Chandrasekaran <bcsraghul@gmail.com>
2025-04-02 17:15:54 -0400

    fixed aboutus page naming

--- source_code/frontend/src/__tests__/aboutUs.test.js
+++ source_code/frontend/src/__tests__/aboutUs.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Aboutus from '../pages/Aboutus';
-
+import Aboutus from "../pages/Aboutus" 
 jest.mock('../assets/logo.png', () => 'mocked-logo.png');
 
 describe('AboutUs Component', () => {
