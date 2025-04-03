d7252fa70772c36f15576128c516b13637cc76d9
Rithik Sowdermett <srithik2310@gmail.com>
2025-04-03 11:43:56 -0400

    add backend test coverage

--- source_code/backend/routes/userRoutes.js
+++ source_code/backend/routes/userRoutes.js
@@ -27,11 +27,11 @@ let userRoutes = express.Router()
  * Description: Verify user login info. Returns user info upon success.
 */
 
-userRoutes.route("/users").get(async (request, response) => {
-  let db = database.getDb()
-  let data = await db.collection("users").find({}).toArray()
-  response.json({ data })
-})
+// userRoutes.route("/users").get(async (request, response) => {
+//     let db = database.getDb()
+//     let data = await db.collection("users").find({}).toArray()
+//     response.json({ data })
+// })
 
 userRoutes.route("/users/login").post(async (request, response) => {
   try {
